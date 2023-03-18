c35e096c5deb8163ee13f4ff13fd2f2eaf056270
Chen <chenyu.dev@gmail.com>
2023-03-18 23:17:27 +0800

    自定义

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -518,7 +518,7 @@ onUnmounted(() => {
             </span>
           </HoverButton>
           <HoverButton v-if="!isMobile" @click="toggleUsingContext">
-            <span class="text-xl" :class="{ 'text-[#4b9e5f]': usingContext, 'text-[#a8071a]': !usingContext }">
+            <span class="text-xl" :class="{ 'text-[#1890ff]': usingContext, 'text-[#a8071a]': !usingContext }">
               <SvgIcon icon="ri:chat-history-line" />
             </span>
           </HoverButton>
